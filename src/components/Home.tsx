09a62397acf17ce6cbe363c46d4903569acc019c
Hudson Santos <hudsnss@gmail.com>
2022-07-13 06:25:38 -0300

    home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import '../styles/Home.css'
+
+export function Home() {
+
+    return (
+        <section className="Home">
+            <h1>Olá! Eu sou Hudson Santos <br />
+                <span>  Desenvolvedor Front-End</span>
+            </h1>
+        </section >
+    )
+}
