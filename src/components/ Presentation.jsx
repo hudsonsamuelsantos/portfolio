e2323ac808d92cfc7e418ca25f96c6073a4360ac
Hudson Santos <hudsnss@gmail.com>
2022-07-19 22:09:06 -0300

    new components

--- src/components/ Presentation.jsx
+++ src/components/ Presentation.jsx
@@ -1,11 +1,35 @@
+import { GithubLogo, LinkedinLogo } from 'phosphor-react'
 
 import '../styles/Presentation.css'
 
 export function Presentation() {
 
     return (
-        <h1>Olá! Eu sou Hudson Santos <br />
-            <span>Desenvolvedor Front-End</span>
-        </h1>
+        <div className='Presentation'>
+            <div className='MyPresentation'>
+                <h1>Olá! Eu sou Hudson Santos <br />
+                    <span>Desenvolvedor Front-End</span>
+                </h1>
+            </div>
+
+            <div className='Social'>
+
+                <div className='ProfileImage'>
+                    <img src="https://github.com/hudsonsamuelsantos.png" alt="Hudson Santos" />
+                </div>
+
+                <div className='SocialLinks'>
+                    <a href="">
+                        <GithubLogo size={48} color='#ffffff' />
+                        GitHub
+                    </a>
+                    <a href="">
+                        <LinkedinLogo size={48} color='#ffffff' />
+                        LinkedIn
+                    </a>
+                </div>
+            </div>
+
+        </div>
     )
 }
