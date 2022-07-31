bebb4f3f1665d0d0dabba95a248026ac7d5597ff
Hudson Santos <hudsnss@gmail.com>
2022-07-30 22:13:51 -0300

    home animation

--- src/components/ Presentation.jsx
+++ src/components/ Presentation.jsx
@@ -1,14 +1,32 @@
 import { GithubLogo, LinkedinLogo } from 'phosphor-react'
+import { useRef } from 'react'
 
 import '../styles/Presentation.css'
 
 export function Presentation() {
 
+    const title = useRef(null)
+
+    function typeWrite(elemento){
+        const textoArray = elemento.innerText.split('');
+        elemento.innerText = null
+        textoArray.forEach(function(letra, i){   
+          
+        setTimeout(function(){
+            elemento.innerHTML += letra;
+        }, 55 * i)
+    
+        })
+
+
+    }
+
+    { setInterval(function() {title.current != null ? typeWrite(title.current) : null}, 9000) }
     return (
         <div className='Presentation'>
             <div className='MyPresentation'>
-                <h1>Olá!<br />
-                    Eu sou Hudson Santos<br />
+                <h1 ref={title} className='title'>Olá!
+                    Eu sou Hudson Santos
                     Desenvolvedor Front-End
                 </h1>
             </div>
