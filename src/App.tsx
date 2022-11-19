25cc157cdc5834493077bb18da0452af64d572d8
Hudson Santos <hudsnss@gmail.com>
2022-11-18 21:37:06 -0300

    arm

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import styles from "./app.module.css"
 
+import Arm from "./assets/light-brown-arm.png"
+
 import { Nav } from "./components/nav"
 import { Presentation } from "./components/presentation"
 
 function App() {
   return (
     <div className={styles.app}>
+      <img className={styles.arm} src={Arm} />
+
       <div className={styles.container}>
         <header>
           <nav className={styles.nav}>
