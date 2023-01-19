d5d3778a217746d85ce5cc6b527ea7b03f3317b7
Hudson Santos <hudsnss@gmail.com>
2023-01-19 20:59:36 -0300

    barra de navegação

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -10,7 +11,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='bg-blue-700'>hello</main>
+      <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+            <h1 className="font-burtons text-xl">hudson santos</h1>
+
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  className=" cursor-pointer text-2xl"
+                />
+              </li>
+
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
     </>
   )
 }
