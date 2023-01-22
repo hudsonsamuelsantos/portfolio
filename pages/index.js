296ac40512592507fa3ef5e0db28591d57faaff7
Hudson Santos <hudsnss@gmail.com>
2023-01-22 01:08:08 -0300

    botão de download do cv

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,8 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
+                  href="cv-hudson-santos.pdf"
+                  download
                 >
                   Baixar CV
                 </a>
